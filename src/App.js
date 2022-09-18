f1df397867a45ae35ffc2f9577838eeb66e919a8
Katherine Gonchar <katherinegon4ar@gmail.com>
2022-09-18 23:51:45 +0300

    Built a dictionary app with search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
+import Dictionary from "./Dictionary";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+        <div className="container">
+            <header className="App-header">
+            </header>
+            <div className="main">
+                <Dictionary />
+            </div>
+            <footer className="text-center">Coded by Katherine Gonchar</footer>
+        </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import "./Dictionary.css";
+import axios from "axios";
+import Results from "./Results";
+
+export default function Dictionary(){
+    let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
+    function handleResponse(response){
+        setResults(response.data[0]);
+    }
+    function search(event){
+        event.preventDefault();
+        let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiURL).then(handleResponse);
+    }
+    function handleKeywordChange(event){
+        setKeyword(event.target.value);
+    }
+    return (
+        <div className="Dictionary">
+            <form onSubmit={search}>
+                <input type="search" onChange={handleKeywordChange}/>
+            </form>
+            <Results results={results} />
+        </div>
+    )
+}
